5e6d2f1e334d18784b601f15639144552365941a
Brian Gupta <brian.gupta@brandorr.com>
2020-05-28 22:36:36 -0400

    DEVOPS-5053 more DD agent 7 changes

--- manifests/params.pp
+++ manifests/params.pp
@@ -15,30 +15,60 @@
 # Sample Usage:
 #
 class datadog_agent::params {
-  $conf_dir       = '/etc/dd-agent/conf.d'
-  $dd_user        = 'dd-agent'
-  $dd_group       = 'root'
-  $package_name   = 'datadog-agent'
-  $service_name   = 'datadog-agent'
+
+#  $dd_group       = 'root'
+
+# Used for legacy storm integration
   $checks_dir     = '/etc/dd-agent/checks.d'
 
-  $process_default_scrub_args   = true
-  $process_default_custom_words = []
-  $logs_enabled                 = false
-  $logs_open_files_limit        = undef
-  $container_collect_all        = false
-  $legacy_conf_dir              = '/etc/dd-agent/conf.d'
-  $conf_dir                     = '/etc/datadog-agent/conf.d'
-  $conf_dir_purge                 = false
   $datadog_site                   = 'datadoghq.com'
-  $agent_log_file             = '/var/log/datadog/agent.log'
+  $dd_groups                      = undef
+  $default_agent_major_version    = 5
+  $agent_version                  = 'latest'
+  $dogapi_version                 = 'installed'
+  $gem_provider                   = 'puppetserver_gem'
+  $conf_dir_purge                 = false
+  $apt_default_release            = 'stable'
+  $apm_default_enabled            = false
+  $process_default_enabled        = false
+  $process_default_scrub_args     = true
+  $process_default_custom_words   = []
+  $logs_enabled                   = false
+  $logs_open_files_limit          = undef
+  $container_collect_all          = false
+  $use_apt_backup_keyserver       = false
+  $apt_backup_keyserver           = 'hkp://pool.sks-keyservers.net:80'
+  $apt_keyserver                  = 'hkp://keyserver.ubuntu.com:80'
+  $sysprobe_service_name          = 'datadog-agent-sysprobe'
 
   case $::operatingsystem {
     'Ubuntu','Debian' : {
-      $rubydev_package   =  'ruby-dev'
+      $rubydev_package            = 'ruby-dev'
+      $legacy_conf_dir            = '/etc/dd-agent/conf.d'
+      $conf_dir                   = '/etc/datadog-agent/conf.d'
+      $dd_user                    = 'dd-agent'
+      $dd_group                   = 'dd-agent'
+      $service_name               = 'datadog-agent'
+      $agent_log_file             = '/var/log/datadog/agent.log'
+      $package_name               = 'datadog-agent'
+      $permissions_directory      = '0755'
+      $permissions_file           = '0644'
+      $permissions_protected_file = '0600'
+      $agent_binary               = '/opt/datadog-agent/bin/agent/agent'
     }
-    'RedHat','CentOS','Fedora','Amazon','Scientific' : {
-      $rubydev_package   = 'ruby-devel'
+    'RedHat','CentOS','Fedora','Amazon','Scientific','OracleLinux' : {
+      $rubydev_package            = 'ruby-devel'
+      $legacy_conf_dir            = '/etc/dd-agent/conf.d'
+      $conf_dir                   = '/etc/datadog-agent/conf.d'
+      $dd_user                    = 'dd-agent'
+      $dd_group                   = 'dd-agent'
+      $service_name               = 'datadog-agent'
+      $agent_log_file             = '/var/log/datadog/agent.log'
+      $package_name               = 'datadog-agent'
+      $permissions_directory      = '0755'
+      $permissions_file           = '0644'
+      $permissions_protected_file = '0600'
+      $agent_binary               = '/opt/datadog-agent/bin/agent/agent'
     }
     default: { fail("Class[datadog_agent]: Unsupported operatingsystem: ${::operatingsystem}") }
   }
--- manifests/redhat.pp
+++ manifests/redhat.pp
@@ -14,42 +14,78 @@
 # Sample Usage:
 #
 class datadog_agent::redhat(
-  $baseurl = "https://yum.datadoghq.com/rpm/${::architecture}/",
-  $gpgkey = 'https://yum.datadoghq.com/DATADOG_RPM_KEY.public',
+  $agent_major_version = $datadog_agent::params::default_agent_major_version,
   $manage_repo = true,
-  $agent_version = 'latest'
-) {
+  $agent_version = $datadog_agent::params::agent_version,
+) inherits datadog_agent::params {
 
+  validate_integer($agent_major_version)
   validate_bool($manage_repo)
+  validate_string($agent_version)
+
   if $manage_repo {
-    validate_string($baseurl)
+
+    case $agent_major_version {
+      5 : {
+        $defaulturl = "https://yum.datadoghq.com/rpm/${::architecture}/"
+        $gpgkey = 'https://yum.datadoghq.com/DATADOG_RPM_KEY.public'
+      }
+      6 : {
+        $defaulturl = "https://yum.datadoghq.com/stable/6/${::architecture}/"
+        $gpgkey = 'https://yum.datadoghq.com/DATADOG_RPM_KEY.public'
+      }
+      7 : {
+        $defaulturl = "https://yum.datadoghq.com/stable/7/${::architecture}/"
+        $gpgkey = 'https://yum.datadoghq.com/DATADOG_RPM_KEY_E09422B3.public'
+      }
+      default: { fail('invalid agent_major_version') }
+    }
+
+    $baseurl = $defaulturl
+
+    $public_key_local = '/etc/pki/rpm-gpg/DATADOG_RPM_KEY.public'
+
+    file { 'DATADOG_RPM_KEY_E09422B3.public':
+        owner  => root,
+        group  => root,
+        mode   => '0600',
+        path   => $public_key_local,
+        source => 'https://yum.datadoghq.com/DATADOG_RPM_KEY_E09422B3.public'
+    }
+
+    exec { 'install-gpg-key':
+        command => "/bin/rpm --import ${public_key_local}",
+        onlyif  => "/usr/bin/gpg --dry-run --quiet --with-fingerprint -n ${public_key_local} | grep 'A4C0 B90D 7443 CF6E 4E8A  A341 F106 8E14 E094 22B3' || gpg --dry-run --import --import-options import-show ${public_key_local} | grep 'A4C0B90D7443CF6E4E8AA341F1068E14E09422B3'",
+        unless  => '/bin/rpm -q gpg-pubkey-e09422b3',
+        require => File['DATADOG_RPM_KEY_E09422B3.public'],
+    }
+
+    yumrepo { 'datadog-beta':
+      ensure => absent,
+    }
+
+    yumrepo {'datadog5':
+      ensure   => absent,
+    }
+
+    yumrepo {'datadog6':
+      ensure   => absent,
+    }
 
     yumrepo {'datadog':
-      enabled  => 0,
+      enabled  => 1,
       gpgcheck => 1,
       gpgkey   => $gpgkey,
       descr    => 'Datadog, Inc.',
       baseurl  => $baseurl,
+      require  => Exec['install-gpg-key'],
     }
 
-    Package { require => Yumrepo['datadog']}
+    Package { require => Yumrepo['datadog6']}
   }
 
-  package { 'datadog-agent-base':
-    ensure => absent,
-    before => Package['datadog-agent'],
-  }
-
-  package { 'datadog-agent':
+  package { $datadog_agent::params::package_name:
     ensure  => $agent_version,
-  }
-
-  service { 'datadog-agent':
-    ensure    => $::datadog_agent::service_ensure,
-    enable    => $::datadog_agent::service_enable,
-    hasstatus => false,
-    pattern   => 'dd-agent',
-    require   => Package['datadog-agent'],
   }
 
 }
