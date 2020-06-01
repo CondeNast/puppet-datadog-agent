0dcd5da887f38802058a1e975ccd362bb6a3060e
Brian Gupta <brian.gupta@brandorr.com>
2020-06-01 19:29:25 -0400

    DEVOPS-5053 Fix puppet version

--- manifests/service.pp
+++ manifests/service.pp
@@ -5,27 +5,17 @@
 
 class datadog_agent::service(
   $service_ensure = 'running',
-  Boolean $service_enable = true,
-  Optional[String] $service_provider = undef,
+  $service_enable = true,
 ) inherits datadog_agent::params {
 
-  if $service_provider {
-    service { $datadog_agent::params::service_name:
-      ensure    => $service_ensure,
-      enable    => $service_enable,
-      provider  => $service_provider,
-      hasstatus => false,
-      pattern   => 'dd-agent',
-      require   => Package[$datadog_agent::params::package_name],
-    }
-  } else {
-    service { $datadog_agent::params::service_name:
-      ensure    => $service_ensure,
-      enable    => $service_enable,
-      hasstatus => false,
-      pattern   => 'dd-agent',
-      require   => Package[$datadog_agent::params::package_name],
-    }
+  validate_bool($service_enable)
+
+  service { $datadog_agent::params::service_name:
+    ensure    => $service_ensure,
+    enable    => $service_enable,
+    hasstatus => false,
+    pattern   => 'dd-agent',
+    require   => Package[$datadog_agent::params::package_name],
   }
 
 }
