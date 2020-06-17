6568be091f6c47c3a64b6a7c8576e6b976fe1035
Dmitriy Komarov <dmitriy_komarov@condenast.com>
2020-06-16 23:03:28 -0400

    DEVOPS-5053 update process integration

--- manifests/integrations/process.pp
+++ manifests/integrations/process.pp
@@ -45,7 +45,27 @@ class datadog_agent::integrations::process(
 
   validate_array( $processes )
 
-  file { "${datadog_agent::params::conf_dir}/process.yaml":
+  $legacy_dst = "${datadog_agent::params::legacy_conf_dir}/process.yaml"
+  if $::datadog_agent::_agent_major_version > 5 {
+    $dst_dir = "${datadog_agent::params::conf_dir}/process.d"
+    file { $legacy_dst:
+      ensure => 'absent'
+    }
+
+    file { $dst_dir:
+      ensure  => directory,
+      owner   => $datadog_agent::params::dd_user,
+      group   => $datadog_agent::params::dd_group,
+      mode    => $datadog_agent::params::permissions_directory,
+      require => Package[$datadog_agent::params::package_name],
+      notify  => Service[$datadog_agent::params::service_name]
+    }
+    $dst = "${dst_dir}/conf.yaml"
+  } else {
+    $dst = $legacy_dst
+  }
+
+  file { $dst:
     ensure  => file,
     owner   => $datadog_agent::params::dd_user,
     group   => $datadog_agent::params::dd_group,
