1815e78d465144b540f0463dd77f2005023cc85f
Dmitriy Komarov <dmitriy_komarov@condenast.com>
2020-06-16 22:25:14 -0400

    DEVOPS-5053 updating mongo integration

--- manifests/integrations/mongo.pp
+++ manifests/integrations/mongo.pp
@@ -59,13 +59,33 @@ class datadog_agent::integrations::mongo(
 ) inherits datadog_agent::params {
   include datadog_agent
 
+  $legacy_dst = "${datadog_agent::params::legacy_conf_dir}/mongo.yaml"
+  if $::datadog_agent::_agent_major_version > 5 {
+    $dst_dir = "${datadog_agent::params::conf_dir}/mongo.d"
+    file { $legacy_dst:
+      ensure => 'absent'
+    }
+
+    file { $dst_dir:
+      ensure  => directory,
+      owner   => $datadog_agent::params::dd_user,
+      group   => $datadog_agent::params::dd_group,
+      mode    => $datadog_agent::params::permissions_directory,
+      require => Package[$datadog_agent::params::package_name],
+      notify  => Service[$datadog_agent::params::service_name]
+    }
+    $dst = "${dst_dir}/conf.yaml"
+  } else {
+    $dst = $legacy_dst
+  }
+
   validate_array($servers)
 
   file { "${datadog_agent::params::conf_dir}/mongo.yaml":
     ensure  => file,
     owner   => $datadog_agent::params::dd_user,
     group   => $datadog_agent::params::dd_group,
-    mode    => '0600',
+    mode    => $datadog_agent::params::permissions_protected_file,
     content => template('datadog_agent/agent-conf.d/mongo.yaml.erb'),
     require => Package[$datadog_agent::params::package_name],
     notify  => Service[$datadog_agent::params::service_name]
